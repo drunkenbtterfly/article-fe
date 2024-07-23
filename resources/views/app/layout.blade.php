6b200460045c28e7be1d1ade442688fbb676c0c3
irfan <143263391+drunkenbtterfly@users.noreply.github.com>
2024-07-23 13:08:07 +0700

    update layout

--- resources/views/app/layout.blade.php
+++ resources/views/app/layout.blade.php
@@ -14,10 +14,26 @@
       .menu.active {
         display: block;
       }
+
+      .group:hover .group-hover\\:block {
+        display: block;
+      }
+
+      .group:hover .group-hover\\:hidden {
+          display: none;
+      }
+
+      .group .group-hover\\:block {
+          display: none;
+      }
+
+      .group .group-hover\\:hidden {
+          display: block;
+      }
   </style>
 </head>
 <body>
-<nav class="bg-gradient-to-r from-[#b18de4] to-[#6db4e2] px-4 py-4 md:px-20">
+<nav class="bg-gradient-to-r from-[#b18de4] to-[#6db4e2] px-4 py-4 lg:px-40">
         <div class="flex items-center justify-between">
             <!-- logo youtz -->
             <div class="font-bold text-lg flex items-center">
@@ -37,11 +53,11 @@
         
             <!-- Menu di bagian kanan -->
             <div id="menu" class="hidden md:flex md:items-center md:space-x-4">
-                <a href="#" class="text-2xl font-bold">Incubator</a>
-                <a href="#" class="text-2xl font-bold">FAQ</a>
-                <a href="#" class="text-2xl font-bold">Contact</a>
-                <a href="#" class="text-2xl font-bold ml-10">Contact</a>
-                <a href="#" class="text-black">
+                <a href="#" class="text-xl text-white font-semibold">Incubator</a>
+                <a href="#" class="text-xl text-white font-semibold">FAQ</a>
+                <a href="#" class="text-xl text-white font-semibold">Contact</a>
+                <a href="#" class="text-xl text-white font-semibold ml-10">Contact</a>
+                <a href="#" class="text-white">
                     <svg class="h-8 w-8 inline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35"/>
                         <circle cx="11" cy="11" r="8"/>
@@ -52,16 +68,16 @@
         
         <!-- Menu untuk mobile -->
         <div id="mobile-menu" class="menu md:hidden mt-5">
-            <a href="/latest" class="block text-2xl font-bold py-2 hover:underline">Home</a>
-            <a href="/incubator" class="block text-2xl font-bold py-2 hover:underline">Incubator</a>
-            <a href="/faq" class="block text-2xl font-bold py-2 hover:underline">FAQ</a>
-            <a href="#" class="block text-2xl font-bold py-2 hover:underline">Contact</a>
+            <a href="/latest" class="block text-xl font-semibold text-whiste py-2 text-white hover:text-[#cd3171]">Home</a>
+            <a href="/faq" class="block text-xl font-semibold text-white py-2 hover:text-[#cd3171]">FAQ</a>
+            <a href="#" class="block text-xl font-semibold text-white py-2 hover:text-[#cd3171]">Contact</a>
+            <a href=""></a>
         </div>
     </nav>
     <nav class="p-2 bg-gradient-to-r from-[#cc7ee4] to-[#66c2e7]">
       <div class="flex justify-center items-center">
-        <ul class="flex space-x-[30px] ml-2 md:ml-0 md:space-x-[70px] text-xl md:text-2xl font-bold overflow-x-auto overflow-y-hidden">
-          <li class="text-blue-600 hover:text-blue-600"><a href="/latest">Latest</a></li>
+        <ul class="flex space-x-[30px] ml-2 md:ml-0 md:gap-5 text-xl md:text-xl font-semibold text-white overflow-x-auto overflow-y-hidden">
+          <li class="hover:text-blue-600"><a href="/latest">Latest</a></li>
           <li class="hover:text-blue-600"><a href="/news">News</a></li>
           <li class="hover:text-blue-600"><a href="/entertaiment">Entertaiment</a></li>
           <li class="hover:text-blue-600"><a href="/healt">Healt</a></li>
@@ -69,27 +85,19 @@
           <li class="hover:text-blue-600"><a href="/tech">Tech</a></li>
           <li class="hover:text-blue-600"><a href="/travel">Travel</a></li>
           <li class="hover:text-blue-600"><a href="/automotive">Automotive</a></li>
-          <li class="hover:text-blue-600"><button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-black md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto">More <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-          </svg></button>
-                    <!-- Dropdown menu -->
-                    <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                        <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
-                          <li>
-                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                          </li>
-                          <li>
-                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                          </li>
-                          <li>
-                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                          </li>
-                        </ul>
-                        <div class="py-1">
-                          <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
-                        </div>
-                    </div>
-            </li>
+          <li class="relative group">
+            <a href="#" class="hover:text-blue-600 flex items-center">
+                More
+                <svg class="w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                </svg>
+            </a>
+            <ul class="absolute hidden text-gray-700 pt-1 group-hover:block">
+                <li class=""><a class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="/submenu1">Submenu 1</a></li>
+                <li class=""><a class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="/submenu2">Submenu 2</a></li>
+                <li class=""><a class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="/submenu3">Submenu 3</a></li>
+            </ul>
+        </li>
         </ul>
       </div>
     </nav>
@@ -102,8 +110,8 @@
 
 
 
-    <footer class="p-2">
-      <main class="flex justify-start items-start flex-col gap-3">
+    <footer class="p-5 lg:px-40">
+      <main class="flex justify-start lg:justify-center items-start flex-col lg:flex-row gap-3 lg:gap-[100px]">
         <section class="flex flex-col">
           <img src="{{ asset('images/youtzmedia.png') }}" alt="ym" class="h-20 w-auto">
         </section>
@@ -112,8 +120,8 @@
             <p>SUPPORT</p>
           </div>
           <div class="flex flex-col">
-            <span>FAQ</span>
-            <span>Contact</span>
+            <a href="" class="text-gray-600">FAQ</a>
+            <a href="" class="text-gray-600">Contact</a>
           </div>
         </section>
         <section class="flex flex-col gap-2">
@@ -121,11 +129,11 @@
             <span>LOCATION</span>
           </div>
           <div>
-            <span>Jl. Lengkong Gudang Timur Raya No.79 A, Lengkong Gudang Timur, Kec. Serpong, Kota Tangerang Selatan, Banten 15310</span>
+            <span class="text-gray-600">Jl. Lengkong Gudang Timur Raya No.79 A, Lengkong Gudang Timur, Kec. Serpong, Kota Tangerang Selatan, Banten 15310</span>
           </div>
         </section>
       </main>
-      <hr class="m-8">
+      <hr class="my-8 bg-gray-500">
       <span class="ml-5">Hak Cipta © 2023 Youtz Media</span>
     </footer>
 
@@ -133,7 +141,15 @@
     <script>
       document.getElementById('hamburger').addEventListener('click', function() {
       document.getElementById('mobile-menu').classList.toggle('active');
-    });
+      });
+
+      document.addEventListener('click', function (event) {
+      var dropdown = document.querySelector('.group');
+      if (!dropdown.contains(event.target)) {
+          dropdown.querySelector('ul').classList.add('hidden');
+        }
+      });
+
   </script>
 </body>
 </html>
